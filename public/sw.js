00030bbbcf01b0df98c630f4a37538daed839a9f
rbrignall <rbrignall@gmail.com>
2025-02-15 23:04:11 +0000

    Updates pre v5.6.0

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Increase the v number when the app is updated
 
-const staticCacheName = "byrdle-v5.5.27";
+const staticCacheName = "byrdle-v5.6.0";
 
 const filesToCache = [
 	"./global.css",
--- src/components/widgets/sharebuttons/Twitter.svelte
+++ src/components/widgets/sharebuttons/Twitter.svelte
@@ -9,22 +9,22 @@
   import ShareButton from './ShareButton.svelte';
   let href;
 
-  $: href = encodeURI(`https://twitter.com/intent/tweet/?text=${text}&url=${url}`);
+  $: href = encodeURI(`https://x.com/intent/tweet/?text=${text}&url=${url}`);
 </script>
 
 <style>
 :global(.ssbc-button--twitter) {
-  background-color: #55acee;
+  background-color: #000000;
 }
 
 :global(.ssbc-button--twitter:active),
 :global(.ssbc-button--twitter:hover) {
-  background-color: #2795e9;
+  background-color: #333333;
 }
 </style>
 
 <ShareButton class="ssbc-button--twitter {classes}" {...$$restProps} {ariaLabel} {href}>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <path d="M23.44 4.83c-.8.37-1.5.38-2.22.02.93-.56.98-.96 1.32-2.02-.88.52-1.86.9-2.9 1.1-.82-.88-2-1.43-3.3-1.43-2.5 0-4.55 2.04-4.55 4.54 0 .36.03.7.1 1.04-3.77-.2-7.12-2-9.36-4.75-.4.67-.6 1.45-.6 2.3 0 1.56.8 2.95 2 3.77-.74-.03-1.44-.23-2.05-.57v.06c0 2.2 1.56 4.03 3.64 4.44-.67.2-1.37.2-2.06.08.58 1.8 2.26 3.12 4.25 3.16C5.78 18.1 3.37 18.74 1 18.46c2 1.3 4.4 2.04 6.97 2.04 8.35 0 12.92-6.92 12.92-12.93 0-.2 0-.4-.02-.6.9-.63 1.96-1.22 2.56-2.14z"/>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1227">
+    <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" fill="white"/>
   </svg>
 </ShareButton>
